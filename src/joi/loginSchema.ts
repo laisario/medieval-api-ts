fc827a727e2186663b945c98bc66e6e7303bcc4e
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-12-08 17:05:54 -0300

    mudancas nao comitadas antes

---
+++ src/joi/loginSchema.ts
@@ -0,0 +1,7 @@
+// import Joi from 'joi';
+// import LoginForm from '../interfaces/loginForm';
+
+// const loginSchema = Joi.object({
+//   email: Joi.string().email().required(),
+//   password: Joi.string().min(6).required(),
+// });
